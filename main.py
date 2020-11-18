464e52c2383b5568cc8b3414ca938e52be2718c7
BohdanShaban <bohdanshaban97@gmail.com>
2020-11-18 10:44:14 +0100

    IRIS read && spild in functionef loadDataset()

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import random
 import math
 import operator
 
+
 # READ IRIS && TRAIN/TEST SPLIT		
 
-fileName = 'iris.data'
-split = 0.7
-trainingSet=[]
-testSet=[]
-
-with open(fileName, 'r') as csvfile:
+def loadDataset(filename, split, trainingSet=[] , testSet=[]):
+	with open(filename, 'r') as csvfile:
 	    lines = csv.reader(csvfile)
 	    dataset = list(lines)
 	    for x in range(len(dataset)-1):
@@ -21,6 +18,17 @@ with open(fileName, 'r') as csvfile:
 	        else:
 	            testSet.append(dataset[x])
 
-print('Train set: ' + repr(len(trainingSet)))
-print('Test set: ' + repr(len(testSet)))
 
+
+
+def main():
+	# READ IRIS && TRAIN/TEST SPLIT	
+	trainingSet=[]
+	testSet=[]
+	split = 0.7
+	loadDataset('iris.data', split, trainingSet, testSet)
+	print ('Train set: ' + repr(len(trainingSet)))
+	print ('Test set: ' + repr(len(testSet)))
+
+
+main()
