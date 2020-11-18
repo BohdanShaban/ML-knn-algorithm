c21f0b293354c0b4285ebe2545ff7fb28618aba4
BohdanShaban <bohdanshaban97@gmail.com>
2020-11-18 10:39:12 +0100

    IRIS read and Train/Test split

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-# knn algorithm
+import csv
+import random
+import math
+import operator
+
+# READ IRIS && TRAIN/TEST SPLIT		
+
+fileName = 'iris.data'
+split = 0.7
+trainingSet=[]
+testSet=[]
+
+with open(fileName, 'r') as csvfile:
+	    lines = csv.reader(csvfile)
+	    dataset = list(lines)
+	    for x in range(len(dataset)-1):
+	        for y in range(4):
+	            dataset[x][y] = float(dataset[x][y])
+	        if random.random() < split:
+	            trainingSet.append(dataset[x])
+	        else:
+	            testSet.append(dataset[x])
+
+print('Train set: ' + repr(len(trainingSet)))
+print('Test set: ' + repr(len(testSet)))
+
